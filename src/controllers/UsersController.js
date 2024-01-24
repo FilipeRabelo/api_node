ce498c640f4967b98e3ba644f505f268b9d1f0e4
https://github.com/FilipeRabelo/projeto_fotoBlog_rocketSeat <https://github.com/FilipeRabelo>
2024-01-24 13:19:09 -0300

    api_node

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -2,40 +2,24 @@
     // RESPONSAVEL POR PROCESSAR A REQUISIÇÃO     // REGRAS DE NEGOCIO
     // class permite que dentro dela EXISTA varias funções, a partir de uma class eu posso criar e acessar varias funções
 
+    /* padrao - um controller pode ter no maximo 5 funções/metodos
+        * index - get para listar varios registros. ex: todos os usuarios cadastrados - index. get    
+        * show - GET para existir um registro específico - 2ex: um usuario específico
+        * create - POST para criar um registro
+        * update - PUT para atualiza rum registro
+        * delet - DELETE para remover um registro
+        ( padrao do controller - maximo 5 metodos )
+    */
+
     // CRIANDO UMA REQUISIÇÃO // CRIANDO O UsersController - USERS
-    class UsersController {                                         // a class sabe que o create é um novo metodo-funcao
+    // funcao p criar um usuario - class - objeto
 
-        // criando um usuario
-        create(request, response){                                  // CRIANDO request - preciso de uma requisição e uma resposta
-
-            const { name, email, password } = request.body;         // obtendo info do requeste.body - corpo da requisição -desestruturando
-            response.status(201).json({ name, email, password });   // devolvendo atraves de um json as info para o body - info que vao vim pelo corpo da requisição saõ JSON()
-        }
+    class UsersController {                                     // a class sabe que o create é um novo metodo-funcao
+      create(request, response){                                // CRIANDO request - preciso de uma requisição e uma resposta
+        
+        const { name, email, password } = request.body;         // obtendo info do requeste.body - corpo da requisição -desestruturando
+        response.status(201).json({ name, email, password });   // devolvendo atraves de um json as info para o body - info que vao vim pelo corpo da requisição saõ JSON()
+      }
     }
 
     module.exports = UsersController;
-
-
-
-
-
-
-
-
-
-    /* padrao - um controller pode ter no maximo 5 funções/metodos
-
-        * index - get para listar varios registros.
-        ex: todos os usuarios cadastrados - index. get
-
-        * show - GET para existir um registro específico - 2ex: um usuario específico
-
-        * create - POST para criar um registro
-
-        * update - PUT para atualiza rum registro
-
-        * delet - DELETE para remover um registro
-
-        ( padrao do controller - maximo 5 metodos )
-
-    */
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -7,53 +7,40 @@
   const { Router }      = require("express")                          // importação - trazendo o Router de dentro do express
   const UsersController = require("../controllers/UsersController")   // importando o controller
   
-  const usersRoutes     = Router();                                   // INICIALIZANDO O Router
+  const usersRoutes     = Router();                                   // aqui esta as rotas // INICIALIZANDO O Router
   
-  // middleware de autentificação
-  function myMiddlewere (request, response, next){      // conseguimos extrair a requisição, a resposta e o next (destino da requisição)
-    console.log("Voce passou pelo middleware");
-    console.log(request.body);                          // para acessar o corpo da requisição
-   
-    if(!request.body.admin){                            // se for diferente de true -  se for false
-      return response.json({ message: "Msg do middleware - Acesso negado"}) // return para parar a funcao - message de retorno pelo middleware
-    }
-
-    next()                                              // funcao do middleware que chama a proxima funcao - DESTINO
-  }
-
   const usersController = new UsersController();                      // fazendo uma new - nova instacia
 
-  // rota - funcao para criar (create) um usuario
-  usersRoutes.post("/", myMiddlewere,  usersController.create);       // rota 1° - criando o usuario - chamando o crete de dentro do controller
+  usersRoutes.post("/",  usersController.create);  // rota - funcao para criar (create) um usuario criando users - chamando o crete de dentro do controller
 
   module.exports        = usersRoutes;                                // EXPORTANDO esse arquivo para o server.js e todo mundo que quiser usar //
 
 
 
-
-
-
-
-
-
-
-
+  
 
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
   // middleware
   // criando uma funcao middleware p intecpeta a requisição e faz autentificações
 
-  // function myMiddleware (request, response, next){          // consigo acessar a requisição, resposta e destino //
-  //
-  //   console.log("Voce passou pelo middlerewere")
-  //
-  //   if(!request.body.isAdmin){                              // validação, se for diferente do admin, message nao autorizado
-  //     return response.json({ message: "Nao autorizado" })   // return intemrrompe
-  //   }
-  //
-  //   next()                                                  // chama o destino - usersController.create)
-  // }
+  // usersRoutes.use(myMiddlewere); // para colocar para todas as rotas
+
+    // middleware de autentificação
+    // function myMiddlewere (request, response, next){      // conseguimos extrair a requisição, a resposta e o next (destino da requisição)
+    //   console.log("Voce passou pelo middleware");
+    //   console.log(request.body);                          // para acessar o corpo da requisição
+     
+    //   if(!request.body.admin){                            // se for diferente de true -  se for false
+    //     // resposta
+    //     return response.json({ message: "Msg do middleware - Acesso negado"}) // return para parar a funcao - message de retorno pelo middleware
+    //   }
+  
+    //   next()                                              // funcao do middleware que chama a proxima funcao - DESTINO
+    // }
+
+
+
 
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
